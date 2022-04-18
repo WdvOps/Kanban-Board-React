b6c3410ee200c0ea3afb2316cec64cffca8832c7
Will <dvops@outlook.com>
2022-04-17 22:00:52 -0300

    Feat: Added function add tasks/ props and states

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Navbar from "./components/Navbar/Navbar";
 import TaskList from "./components/TaskList/TaskList";
 
+const task = {
+  id: 0,
+  title: "Nova Tarefa",
+  state: "pendendte",
+};
+
+// Gera novo id
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+
+  return idAcc;
+};
+
 function App() {
+  const [tasks, setTask] = useState([]);
+
+  const addTask = (title, state) => {
+    console.log("Get from App");
+    const newTask = {
+      id: generateId(),
+      title,
+      state,
+    };
+
+    setTask((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <div>
-        <TaskList title="Iniciada" />
-        <TaskList title="Executando" />
-        <TaskList title="Pendente" />
-        <TaskList title="Finalizada" />
+      <div className="container">
+        <TaskList title={`Agendada`} onAddTask={addTask} tasks={tasks} />
       </div>
     </div>
   );
--- kanban-app/src/components/TaskList/TaskList.js
+++ kanban-app/src/components/TaskList/TaskList.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./tasklist.css";
 
-export default function TaskList({ title }) {
+import PropTypes from "prop-types";
+
+export default function TaskList({ title, onAddTask, tasks }) {
+  const addTask = () => {
+    console.log("Get from TaskList");
+    onAddTask("Nova Tarefa", "Pendente");
+  };
+
   return (
     <div className="tasklist">
       <div className="title">{title}</div>
-      <div className="content"></div>
+      <div className="content">
+        {tasks.map((task) => {
+          return <div key={task.id}>{task.title}</div>;
+        })}
+      </div>
+      <button onClick={addTask}>Add visit</button>
     </div>
   );
 }
+
+// Define o tipo de dado(string) e preenchimento obrigtório
+TaskList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired,
+};
